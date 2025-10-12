cb62601a7090954eb5a600c646f2e970675ef218
Vito <marzoccavito01@gmail.com>
2025-10-12 21:02:59 +0200

    completamento menu di accensione e spegnimento

--- BluetoothManger.sh
+++ BluetoothManger.sh
@@ -1,8 +1,67 @@
 #! /bin/bash
+tput civis
+
+menuPrincipale()
+{
+dialog --menu "BluetoothManager TUI" 12 35 4 \
+"Status" "" \
+"Connect" "" \
+"Modify Connection" "" \
+" " "" \
+"Exit" "" \
+3>&1 1>&2 2>&3
+}
 
 
-whiptail --title "leutoothManager TUI" --menu "" 12 35 3 \
-"Status" " " \
-"Connect" " " \
-"Modify connection" " "
+
+menuStatus()
+{
+scelta=$(dialog --menu "Chose the status" 12 35 2 \
+"ON" "" "OFF" "" 3>&1 1>&2 2>&3) || return
+
+if [ "$scelta" = "ON" ]; then
+  bluetoothctl power on  >/dev/null 2>&1
+else
+  bluetoothctl power off >/dev/null 2>&1
+fi
+
+
+}
+
+menuConnect()
+{
+stato="$(bluetoothctl show | grep -Po '^\s*PowerState:\s*\K(on|off)')"
+
+if [ "$stato" == "off" ]
+then
+dialog --title "Warning" --msgbox "Bluetooth disabled.\nEnable it to run a scan." 8 50
+fi
+
+
+}
+
+
+
+while true
+do
+scelta=$(menuPrincipale)
+
+if [ "$scelta" = "Status" ]
+then
+  menuStatus
+  
+elif [ "$scelta" == "Connect" ]
+then
+menuConnect
+	
+  
+elif [ "$scelta" = "Exit" ]
+then
+  break
+  
+fi
+done
+
+
+done
 

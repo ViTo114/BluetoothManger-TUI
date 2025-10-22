c96997789961102fff12df8830df399a3708f32c
Vito <marzoccavito01@gmail.com>
2025-10-22 21:34:00 +0200

    miglioramenti generali

--- Main.py
+++ Main.py
@@ -8,14 +8,18 @@ import os
 def controllaStato() -> bool:
     stato = False
 
-    output = subprocess.run("bluetoothctl show", text=True, capture_output=True, shell=True )
+    comando = "bluetoothctl show"
+
+    output = subprocess.run(comando, text=True, capture_output=True, shell=True )
 
     if "PowerState: on" in output.stdout:
         stato = True
 
     return stato
 
-def mostraStato(app) -> None:
+def statoBluetooth(app) -> None:
+    app.statoCorrente = Static()
+
     stato = "Bluetooth is "
 
     if controllaStato():
@@ -32,18 +36,24 @@ def mostraStato(app) -> None:
     app.statoCorrente.styles.padding = 1
     app.statoCorrente.styles.margin = 3
 
+    app.mount(app.statoCorrente)
+
 def menuPrincipale(app) -> None:
+    app.menuPrincipale = ListView(
+        ListItem(Label("Stato")),
+        ListItem(Label("Connessione"))
+    )
+
     app.menuPrincipale.styles.border = ("heavy", "white")
     app.menuPrincipale.border_title = "Bluetooth Manger"
     app.menuPrincipale.styles.width = 30
     app.menuPrincipale.styles.height = 7
     app.menuPrincipale.styles.margin = 3
 
-def mostraMenuStato(app) -> None:
-    app.menuCorrente = 2
+    app.mount(app.menuPrincipale)
 
-    app.menuPrincipale.display = False
-    app.statoCorrente.display = False
+def menuStato(app) -> None:
+    app.menuCorrente = 2
 
     app.menuStato = (ListView(
         ListItem(Label("ON")),
@@ -58,6 +68,37 @@ def mostraMenuStato(app) -> None:
     app.mount(app.menuStato)
 
     app.menuStato.focus()
+
+def loadingScreen(app) -> None:
+    app.loadingScreen = Static("Scannign for devices ...")
+    app.loadingScreen.styles.border = ("heavy", "white")
+
+    app.mount(app.loadingScreen)
+
+def scanDispositiviBluetooth() -> list:
+    comando = "bluetoothctl --timeout 20 scan on"
+
+    output = subprocess.run(comando, text=True, shell=True, capture_output=True)
+
+    deviceList = output.stdout.split("\n")
+
+    return deviceList
+
+def menuListaDevice(app) -> None:
+    app.menuCorrente = 3
+
+    devices = scanDispositiviBluetooth()
+
+    app.listaDevice = ListView()
+
+    for index in devices:
+        app.listaDevice.append(ListItem(Label(devices[index])))
+
+    app.loadingScreen.remove()
+    app.mount(app.listaDevice)
+
+    app.listaDevice.focus()
+
 
 
 
@@ -76,34 +117,26 @@ class MyApp(App):
     }
     """
 
-    def compose(self):
-        self.menuPrincipale = ListView(
-            ListItem(Label("Stato")),
-            ListItem(Label("Connessione"))
-        )
-        yield self.menuPrincipale
-
-        self.statoCorrente = Static()
-        yield  self.statoCorrente
-
-
 
     def on_mount(self):
         menuPrincipale(self)
-        mostraStato(self)
+        statoBluetooth(self)
 
 
     def on_key(self, event):
         if event.key == "enter" and self.menuPrincipale.index == 0 and self.menuCorrente == 1:
-            mostraMenuStato(self)
+            self.menuPrincipale.remove()
+            self.statoCorrente.remove()
+
+            menuStato(self)
 
         elif event.key == "enter" and self.menuCorrente == 2 and self.menuStato.index == 0:
             os.system("bluetoothctl power on >/dev/null 2>&1")
 
             self.menuStato.remove()
-            self.menuPrincipale.display = True
-            mostraStato(self)
-            self.statoCorrente.display = True
+
+            menuPrincipale(self)
+            statoBluetooth(self)
 
             self.menuPrincipale.focus()
 
@@ -113,14 +146,21 @@ class MyApp(App):
             os.system("bluetoothctl power off >/dev/null 2>&1")
 
             self.menuStato.remove()
-            self.menuPrincipale.display = True
-            mostraStato(self)
-            self.statoCorrente.display = True
+
+            menuPrincipale(self)
+            statoBluetooth(self)
 
             self.menuPrincipale.focus()
 
             self.menuCorrente = 1
 
+        elif event.key == "enter" and self.menuCorrente == 1 and self.menuPrincipale.index == 1:
+            self.menuPrincipale.remove()
+            self.statoCorrente.remove()
+
+            loadingScreen(self)
+
+            menuListaDevice(self)
 
 
 if __name__ == "__main__":
